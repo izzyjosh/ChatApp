16ed9b9e1048962dbc556a553ca5080ef36fe2ef
izzyjosh <calebayuba222@gmail.com>
2023-12-20 00:04:24 +0100

    Provided url for the chatroom view

--- main/urls.py
+++ main/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "main"
-urlpatterns = []
+urlpatterns = [
+        path("",views.index,name="index"),
+        path("chatroom/<slug:slug>/",views.chatroom,name="chatroom"),
+        ]
