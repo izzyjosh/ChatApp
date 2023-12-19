d49b58404d62fa583326d2e5c0853e29f0c80134
izzyjosh <calebayuba222@gmail.com>
2023-12-20 00:03:41 +0100

    Added message models

--- main/models.py
+++ main/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
-class Personchat(models.Model):
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    sender = models.ForeignKey(User,on_delete=models.CASCADE)
+    message = models.TextField()
+    receiver = models.ForeignKey(ChatRoom,on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
+
+    class Meta:
+        ordering = ("date",)
 
