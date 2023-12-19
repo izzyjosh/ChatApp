7bcf4ac0a539109043a6868ca3f9601598b4850d
izzyjosh <calebayuba222@gmail.com>
2023-12-20 00:03:15 +0100

    Added chatroom view that handles sending of messages

--- main/views.py
+++ main/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render,redirect
 from django.http import HttpRequest
+from .models import ChatRoom,Message
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def index(request:HttpRequest):
-    return render(request,"main/index.html")
+
+    chatrooms = ChatRoom.objects.all()
+    context = {"chatrooms":chatrooms}
+    return render(request,"main/index.html",context)
+
+
+@login_required
+def chatroom(request:HttpRequest,slug):
+    chatroom = ChatRoom.objects.get(slug=slug)
+    messages = Message.objects.all()
+
+    context = {"chatroom":chatroom,"messages":messages}
+    return render(request,"main/chatroom.html",context)
